d0695a2c1075095ea6ab35c9748fef0265d43307
Shraddhanand <1shraddhanand@gmail.com>
2023-04-13 09:37:15 +0530

    profile- button- price

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@
 import Gallery from "./components/gallery/Gallery";
 import Tags from "./components/tags/Tags";
 import Slider_one from "./components/slider1/Slider_one";
+import Price_button from "./components/price_button/Price_button";
 
 
 function App() {
   return (
     <>
       <Gallery />
+      <Price_button />
       <Slider_one />
       <Tags />
       {/* <ToggleDiv /> */}
---
+++ src/components/price_button/Price_button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './price_button.css';
+
+
+const Price_button = () => {
+    return (
+        <>
+            <div className='price-button'>
+                See Price And Book
+            </div>
+        </>
+    )
+}
+
+export default Price_button
